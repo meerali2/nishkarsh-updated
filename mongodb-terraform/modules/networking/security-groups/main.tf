1e6784fe9d68667542fbfa27130815baf8ebca91
meerali2 <meeralan6989@gmail.com>
2025-06-27 19:55:15 +0530

    Create main.tf

---
+++ mongodb-terraform/modules/networking/security-groups/main.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "bastion_sg" {
+  vpc_id = var.vpc_id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "bastion-sg"
+  }
+}
+
+resource "aws_security_group" "mongodb_sg" {
+  vpc_id = var.vpc_id
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "mongodb-sg"
+  }
+}
