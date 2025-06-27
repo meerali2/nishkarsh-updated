c477e401d812baf75c6f5bb07f0e12778c5e0466
meerali2 <meeralan6989@gmail.com>
2025-06-27 19:46:01 +0530

    Create main.tf

---
+++ mongodb-terraform/modules/networking/subnets/main.tf
@@ -0,0 +1,20 @@
+resource "aws_subnet" "public_subnets" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  tags = {
+    Name = "private-subnet-${count.index}"
+  }
+}
