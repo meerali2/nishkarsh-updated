d102314862a4df2ac09cb4e7b1d606ca29b2999a
meerali2 <meeralan6989@gmail.com>
2025-06-27 19:51:33 +0530

    Create variables.tf

---
+++ mongodb-terraform/modules/networking/route-table/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "igw_id" {
+  description = "ID of the Internet Gateway"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
