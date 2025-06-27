3a7d0f06a7046a68eca052935fd4870a479973eb
meerali2 <meeralan6989@gmail.com>
2025-06-27 19:33:42 +0530

    Update outputs.tf

--- mongodb-terraform/outputs.tf
+++ mongodb-terraform/outputs.tf
@@ -1,7 +1,11 @@
 output "vpc_id" {
-  value = module.networking.vpc_id
+  value = module.vpc.vpc_id
 }
 
 output "bastion_host_public_ip" {
   value = module.bastion_host.bastion_host_public_ip
+}
+
+output "mongodb_instance_ids" {
+  value = module.mongodb_servers.mongodb_instance_ids
 }
